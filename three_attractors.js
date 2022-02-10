42e71b1d92d2f5bd22d044dcd67a50527be2c1c4
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2022-02-10 20:48:12 +0100

    add line up wip

--- three_attractors.js
+++ three_attractors.js
@@ -4,26 +4,7 @@ const line_up_button = document.getElementById('line_up')
 
 let attractor_bodies=[]
 
-one_attractor_button.addEventListener('click', (e)=> {
-    World.remove(world, attractor_bodies)
-    attractor_bodies =  Bodies.circle(600, 300, 10, {
-        isStatic: true,
-        plugin: {
-          attractors: [
-            function(bodyA, bodyB) {
-                    return {
-                        x: (bodyA.position.x - bodyB.position.x) * 1e-6,
-                        y: (bodyA.position.y - bodyB.position.y) * 1e-6,
-                      };
-            }
-          ]
-        }
-      });
 
-    World.add(world, attractor_bodies);
-    Engine.update(engine);
-    updateBodies()
-})
 
 three_attractor_button.addEventListener('click', (e)=> {
     World.remove(world, attractor_bodies)
